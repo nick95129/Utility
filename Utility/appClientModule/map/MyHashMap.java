e61e5e7c05a94845a7d539546330c3b969633aea
Nick <Nick@Nick-PC>
2014-08-16 13:38:57 -0700

    add myHashMap

---
+++ Utility/appClientModule/map/MyHashMap.java
@@ -0,0 +1,22 @@
+package map;
+
+public class MyHashMap <K, V>{
+	public  MyHashMap() {}
+	
+	Entry<K,V>[] tables;
+
+	
+	public static class Entry<K,V> {
+		int hash;
+		K key;
+		V value;
+		Entry<K,V> next;
+		
+		public Entry(int h, K k, V v, Entry<K,V> e) {
+			this.hash = h;
+			this.key = k;
+			this.value = v;
+			this.next = e;						
+		}
+	}
+}
